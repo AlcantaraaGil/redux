5995466f9219f2cee22a940c629e01d9f8b2f304
Gilberto Alcantara Ascencio <gilberto.alcantara@officedepot.com.mx>
2024-06-13 16:54:13 -0600

    pokemon api sin rtk query

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { PokemonAPP } from './PokemonAPP';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      {/* <App /> */}
+      <PokemonAPP/>
+    </Provider>
+  </React.StrictMode>
 );
